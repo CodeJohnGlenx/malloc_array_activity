a039253c5f781d9ea6874074246495c991c5ffc4
John <glenfalceso@gmail.com>
2021-05-19 11:17:34 +0800

    malloc pointer school activity program

---
+++ pointer.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "sort.c"
+
+int binary_search(int search, int array[], int array_length)
+{
+    int left = 0, right = array_length - 1, mid = (left + right) / 2;
+    for(;left <= right; mid = (left + right) / 2)
+        if (search == array[mid])
+            return mid;
+        else if (search < array[mid])
+            right = mid - 1;
+        else if (search > array[mid])
+            left = mid + 1;
+    return -1;
+}
+
+void main()
+{
+    int size, *x, i, value, found;
+    char try_again;
+    printf(">>\n");
+    printf("How many values you want to enter? ");
+    scanf("%d", &size);
+    x = (int*) malloc(size * sizeof(int));
+    for (int i = 0; i < size; ++i)
+    {
+        printf("Enter value for (x + %d): ", i);
+        scanf("%d", (x + i));
+    }
+
+    insertion_sort(x, size);
+
+    input:
+        printf("\n\nEnter Key: ");
+        scanf("%d", &value);
+
+    printf("\n");
+    found = binary_search(value, x, size);
+    for (i = 0; i < size; i++)
+        printf("(x + %d): %d\n", i, *(x + i));
+    if (found != -1)
+        printf("%d is located at (x + %d)", value, found);
+    else
+        printf("%d is not found", value);
+
+    printf("\n\nDo you want to enter key again (y/n)? ");
+    scanf(" %c", &try_again);
+
+    if (tolower(try_again) == 'y')
+        goto input;
+
+    printf("\n<<");
+}
---
+++ sort.c
@@ -0,0 +1,16 @@
+void swap(int *num1, int *num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+
+void insertion_sort(int array[], int array_length)
+{
+    for (int i = 0; i < array_length; i++)
+        for(int j = i; j > 0; j--)
+            if (array[j] < array[j-1])
+                swap(&array[j], &array[j-1]);
+            else
+                break;
+}
